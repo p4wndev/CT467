7dd8889dd62ddae0a757a1917b7c90e7a706ea69
b2207517 <datb2207517@student.ctu.edu.vn>
2025-06-17 01:20:36 +0700

    them bang bapnuoc, phim, phongchieu

--- app/backend/server.js
+++ app/backend/server.js
@@ -4,8 +4,11 @@ require("dotenv").config();
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes"); // ⬅️ Thêm dòng này
 const gheRoutes = require("./routes/ghe.routes");
-const suatChieuRoutes = require('./routes/suatChieu.routes');
-const veRoutes = require('./routes/ve.routes');
+const suatChieuRoutes = require("./routes/suatChieu.routes");
+const veRoutes = require("./routes/ve.routes");
+const movieRoutes = require("./routes/movie.routes");
+const bapNuocRoutes = require("./routes/bapnuoc.routes");
+const phongChieuRoutes = require("./routes/phongchieu.routes");
 const app = express();
 app.use(express.json());
 
@@ -15,10 +18,13 @@ app.use("/users", userRoutes); // ⬅️ Đăng ký route người dùng
 app.use("/ghe", gheRoutes);
 app.use("/suatChieu", suatChieuRoutes);
 app.use("/ve", veRoutes);
+app.use("/movies", movieRoutes);
+app.use("/bapnuoc", bapNuocRoutes);
+app.use("/phongchieu", phongChieuRoutes);
 
 // Xử lý lỗi 404 (Not Found)
 app.use((req, res, next) => {
-  res.status(404).json({ message: 'Route không tồn tại.' });
+  res.status(404).json({ message: "Route không tồn tại." });
 });
 
 const port = process.env.PORT || 3000;
